fb976f54d68207c56de3015f876ceb947bbbdd92
thewunmiolabisi <Motlaw2020@gmail.com>
2024-03-31 15:00:13 +0100

    New commit

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,10 +1,28 @@
+import React from "react";
 import Header from "../components/Header";
-
+import styles from "./Shop.module.css";
 export default function ShopPage() {
   return (
-    <div>
+    <>
       <Header />
-      <h1>hey, you're in the shop 🛒🛍</h1>
-    </div>
+      <div className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <p>Original</p>
+          <h1 className={styles.heroTitle}>
+            Non-tarnishing Gold Polished Earring
+          </h1>
+          <p className={styles.heroPrice}>#10,000</p>
+          <p className={styles.heroOffer}>LIMITED TIME OFFER</p>
+          <button className={styles.heroButton}>SHOP NOW</button>
+        </div>
+        <div className={styles.heroImageWrapper}>
+          <img
+            src="../../assets/ProductDisplay.svg"
+            alt="Gold Polished Earring"
+            className={styles.heroImage}
+          />
+        </div>
+      </div>
+    </>
   );
 }
